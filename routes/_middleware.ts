0695d88ce3543c6676d5d037ef3b6e77a83d4f49
Chris Knight <cknight1234@gmail.com>
2023-06-26 07:56:22 +0100

    fix: only log referrers with value

--- routes/_middleware.ts
+++ routes/_middleware.ts
@@ -1,15 +1,16 @@
 import { MiddlewareHandlerContext } from "$fresh/server.ts";
 
 export async function handler(req: Request, ctx: MiddlewareHandlerContext<unknown>,) {
-  const start = Date.now();
+  //const start = Date.now();
   const resp = await ctx.next();
 
   const path = new URL(req.url).pathname;
   if (path === "/") {
-    const referrer = req.headers.get("referer") || 'no-referer';
     //const region = Deno.env.get("DENO_REGION") || 'no-region';
     //const processTime = Date.now() - start;
-    console.log("Referrer: ", referrer);
+    if (req.headers.get("referer")) {
+      console.log("******* Referrer: ", req.headers.get("referer"));
+    }
   }
 
   return resp;
